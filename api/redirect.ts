f5bbe978fa7c318eafd4054addeea95de882a94b
albu-alex <alexmhh1234@gmail.com>
2026-02-21 00:22:48 +0200

    refactor: clean up redirect handler and remove unused logging code

--- api/redirect.ts
+++ api/redirect.ts
@@ -6,7 +6,6 @@ export default async function handler(
 ) {
   const { target, source } = req.query;
 
-  // List of allowed redirect targets
   const allowedTargets: { [key: string]: string } = {
     'google-reviews': 'https://www.google.com/search?sca_esv=a420dcef264fc5ad&tbm=lcl&sxsrf=AE3TifPiQBSvRxmNeGro4O_nsZzQcPG1XA:1760021518520&q=Hookah+Tabacu+Recenzii&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxIxNDMxtDC2NDAwMTczszQyMLQ0MtrAyPiKUcwjPz87MUMhJDEpMblUISg1OTWvKjNzESsOCQCs8uH2TQAAAA&rldimm=16418390047669201922&hl=ro-RO&sa=X&ved=2ahUKEwiK9qGLr5eQAxWL87sIHQK3KIIQ9fQKegQIVBAF&biw=2056&bih=1290&dpr=2#lkt=LocalPoiReviews',
     'tripadvisor': 'https://www.tripadvisor.com/Restaurant_Review-g298474-d26352926-Reviews-Hookah_Tabacu-Cluj_Napoca_Cluj_County_Northwest_Romania_Transylvania.html',
@@ -27,16 +26,5 @@ export default async function handler(
     ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
   });
 
-  // TODO: Add database logging for analytics
-  // Example Firebase logging:
-  // await admin.firestore().collection('redirects').add({
-  //   target,
-  //   source,
-  //   timestamp: admin.firestore.FieldValue.serverTimestamp(),
-  //   userAgent: req.headers['user-agent'],
-  //   ip: req.headers['x-forwarded-for'],
-  // });
-
-  // Redirect to the target URL
   res.redirect(redirectUrl);
 }
